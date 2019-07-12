c23e2a7dc85a3e8e7befd046923ba92804e6f595
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 03:49:28 -0500

    Added small text example

--- Scripts/variables.rb
+++ Scripts/variables.rb
@@ -5,3 +5,12 @@ puts "There once was a man named George"
 puts "he was 70 years old."
 puts "He really liked the name George"
 puts "but he didn't like being 70."
+
+# Using variables
+character_name = "Omar"
+character_age = "20"
+
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+puts ("He really liked the name " + character_name)
+puts ("but he didn't like being " + character_age + ".")
