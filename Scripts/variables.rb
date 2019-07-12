91de81fb81dfc16a0d2ace6b8a807345f3e7b08f
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 03:44:57 -0500

    Added a small text

--- Scripts/variables.rb
+++ Scripts/variables.rb
@@ -1,1 +1,7 @@
 #!/usr/bin/env ruby
+
+# Example text
+puts "There once was a man named George"
+puts "he was 70 years old."
+puts "He really liked the name George"
+puts "but he didn't like being 70."
