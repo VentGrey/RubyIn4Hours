21851b07cf789e1aa08e28a54d50ff1a4210697d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 03:59:59 -0500

    Finished basic data types

--- Scripts/datatypes.rb
+++ Scripts/datatypes.rb
@@ -2,3 +2,11 @@
 
 name = "Omar" # String: An infinite array of characters
 age = 20 # Integer: A whole number without floating points
+gpa = 3.2 # Float: A number with floating points
+is_male = true # Boolean: Can be either true or false
+is_tall = false
+flaws = nil # Null: Doesn't exist (is not a 0)
+
+puts name + "is a " + age + " years old guy."
+puts "it is " + is_male + " that he is a male. But it is " + is_tall + " that he is tall."
+puts "His gpa is: " + gpa + " and his flaws are:" + flaws
