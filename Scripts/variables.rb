fe4cd454ee076bad1f8f7a2825dc48f2cfcc26b1
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 03:52:09 -0500

    Added overwriting example

--- Scripts/variables.rb
+++ Scripts/variables.rb
@@ -14,3 +14,15 @@ puts ("There once was a man named " + character_name)
 puts ("he was " + character_age + " years old.")
 puts ("He really liked the name " + character_name)
 puts ("but he didn't like being " + character_age + ".")
+
+# Overwriting variables
+character_name = "Omar"
+character_age = "20"
+
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+
+character_name = "Vent"
+
+puts ("He really liked the name " + character_name)
+puts ("but he didn't like being " + character_age + ".")
