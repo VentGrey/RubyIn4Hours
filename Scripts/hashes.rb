7b5b6ee9c1accc0820e5ece669e35551d0cb0c90
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 22:46:58 -0500

    Added hashes file

---
+++ Scripts/hashes.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+states = {
+  "Guanajuato" => "Gto",
+  "Querétato" => "Qro",
+  "Monterrey" => "Mty",
+  "Yucatán" => "Yct"
+}
+
+puts states["Guanajuato"] #prints "GTO"
