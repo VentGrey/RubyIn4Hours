e8a392892556e43d76c0eb7b6621985941d90dd6
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 21:59:07 -0500

    Finished with strings

--- Scripts/strings.rb
+++ Scripts/strings.rb
@@ -11,9 +11,11 @@ puts phrase
 puts phrase.upcase() # "HOLA MUNDO"
 puts phrase.downcase() # "hola mundo"
 
-# Strip
 quote = "      Hola Mundo        "
 puts quote.strip() # Removes trailing whitespaces
 puts quote.lenght() # Returns how many characters are in a string
 
 puts quote.include? "Mundo" # Searchs for a string inside a string
+puts phrase[0] # Prints the first character of the current string
+puts phrase.index("M") # Returns the index of the character
+puts phrase.index("Mundo") # Returns the index of the starting character from a string
