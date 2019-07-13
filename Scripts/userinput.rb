b99172a97244a0a55a8b6e3acf256cfc429e141b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 22:16:35 -0500

    Finished this

--- Scripts/userinput.rb
+++ Scripts/userinput.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
 puts "Enter your name"
-name = gets # Take whatever the user inputs and store it in here
-puts ("Hello " + name)
+name = gets.chomp() # Take whatever the user inputs and store it in here
+# chomp gets rid of the \n when we hit enter.
+puts ("Hello " + name + ", you are cool.")
+puts "Enter your age"
+age = gets.chomp()
+puts ("And you are " + age)
