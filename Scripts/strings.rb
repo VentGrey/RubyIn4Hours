0a57c669d78fbd83922ff941a9291c87b5a05386
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 21:55:23 -0500

    Added more thingies

--- Scripts/strings.rb
+++ Scripts/strings.rb
@@ -6,3 +6,14 @@ puts "Hola\n Mundo" # Newline
 # Using Variables
 phrase = "Hola"
 puts phrase
+
+# String Methods
+puts phrase.upcase() # "HOLA MUNDO"
+puts phrase.downcase() # "hola mundo"
+
+# Strip
+quote = "      Hola Mundo        "
+puts quote.strip() # Removes trailing whitespaces
+puts quote.lenght() # Returns how many characters are in a string
+
+puts quote.include? "Mundo" # Searchs for a string inside a string
