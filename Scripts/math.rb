1bc92b3826880ccdefaa47761e37cd1e2ba4c483
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 22:06:34 -0500

    Added Math methods

--- Scripts/math.rb
+++ Scripts/math.rb
@@ -11,5 +11,12 @@ puts 5 / 3 # Division
 puts 2 ** 3 # Exponentials
 puts 10 % 3 # Module
 
-num = 42
+num = 42.245
 puts ("My favorite number is " + num.to_s) # Convert number to string because ruby is crap
+puts num.abs() # Absolute value
+num.round() # Rounds a decimal number
+num.ceil() # Rounds to the highest number
+num.floor() # Rounds to the lowest number
+
+puts Math.sqrt(36) # Returns the square root of the argument
+puts Math.log(1) # Returns the logarithm of the argument
