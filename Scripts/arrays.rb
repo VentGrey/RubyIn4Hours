0bad6720bcf5cd2647504c90a5a26d3e69204f21
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 22:40:19 -0500

    This is how arrays are supposed to work?

---
+++ Scripts/arrays.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+friends = Array["Kevin", "Karen", "Oscar"]
+puts friends
+puts friends[0] # prints the first index of the array
+
+comrades = Array.new
+comrades[0] = "Omar"
+comrades[8] = "Jair"
+# This creates a SHIT TON OF EMPTY SPACES IN THE MIDDLE
