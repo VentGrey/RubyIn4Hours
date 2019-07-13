05a1f76630fa93d419799c03f2e426c6736bb19c
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-12 22:03:46 -0500

    Examples

---
+++ Scripts/math.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+puts 5 # Prints the number 5
+puts 3.14 # Prints decimal numbers
+puts -42 # The opposite of everything
+
+puts 5 + 9 # Prints the sum of 5 + 9
+puts 5 - 4 # Diff
+puts 5 * 9 # Multiplication
+puts 5 / 3 # Division
+puts 2 ** 3 # Exponentials
+puts 10 % 3 # Module
+
+num = 42
+puts ("My favorite number is " + num.to_s) # Convert number to string because ruby is crap
